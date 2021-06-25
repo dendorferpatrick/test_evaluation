d5f0d9a419f160ad3b7bfe825d2373efff480eca
ge73vow <patrick.dendorfer@tum.de>
2021-06-25 15:33:53 +0200

    add upload files

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,7 @@
 import pandas as pd
-def evaluation():
-    df = pd.read_csv("upload/data/upload.csv")
-    df.to_csv("upload/output/results.csv")
+def evaluation(upload_folder ):
+    df = pd.read_csv(upload_folder+"/data/upload.csv")
+    df.to_csv(upload_folder+"/output/results.csv")
     return 0
 
     
--- run.py
+++ run.py
@@ -8,24 +8,23 @@ from evaluation import evaluation
 
 
 if __name__ == "__main__":
-    # Command line interface:
-    # parser = argparse.ArgumentParser()
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--SUBMISSIONID', required=False, type=str, help='shakey where data is dumped')
+    parser.add_argument('--CHALLENGEID', required=False, type=str, help='ID of the Benchmark')
+    parser.add_argument('--GTFOLDER', required=False, type=str, help="GT folder")
+    parser.add_argument('--UPLOADFOLDER', required=False, type=str, help="Upload folder")
+    parser.add_argument('--SEQUENCEIDS', required=False, type=str, help="Sequences to eval")
+    parser.add_argument('--PYTHONPATH', required=False, type=str, help="Sequences to eval")
 
-    # parser.add_argument('--SUBMISSIONID', required=True, type=str, help='shakey where data is dumped')
-    # parser.add_argument('--CHALLENGEID', required=True, type=str, help='ID of the Benchmark')
-    # parser.add_argument('--GTFOLDER', required=True, type=str, help="GT folder")
-    # parser.add_argument('--UPLOADFOLDER', required=True, type=str, help="Upload folder")
-    # parser.add_argument('--SEQUENCEIDS', required=True, type=str, help="Sequences to eval")
-    # parser.add_argument('--PYTHONPATH', required=True, type=str, help="Sequences to eval")
-
-    # args = parser.parse_args().__dict__
+    args = parser.parse_args().__dict__
     # submission_id = args["SUBMISSIONID"]
     # challenge_id = args["CHALLENGEID"]
     # gt_folder = args["GTFOLDER"]
     # sequence_ids = args["SEQUENCEIDS"].replace(",", " ")
-    # upload_folder = args["UPLOADFOLDER"]
+    upload_folder = args["UPLOADFOLDER"]
     # python_path = args["PYTHONPATH"]
     
-    evaluation = evaluation()
+    evaluation = evaluation(upload_folder)
     
 
